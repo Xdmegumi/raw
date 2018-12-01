1053abe6b05f36cf9305dd275178b29d9809823d
Xdmegumi <ahjxszg@gmail.com>
2018-12-01 20:46:17 +0800

    Update gcy1.sh

--- gcy1.sh
+++ gcy1.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-apt-get update
-apt-get -y install git
-git clone https://github.com/flyzy2005/ss-fly
 wget -N --no-check-certificate "https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/cloudt.sh" && chmod +x cloudt.sh && bash cloudt.sh
-ss-fly/ss-fly.sh -ssr
-sed -ie 's/0.0.0.0/::/g' shadowsocks.json
+wget -N --no-check-certificate https://raw.githubusercontent.com/ToyoDAdoubi/doubi/master/ssr.sh && chmod +x ssr.sh && bash ssr.sh
+bash ssr.sh
+sed -ie 's/0.0.0.0/::/g' /etc/shadowsocks.json
 ssserver -c /etc/shadowsocks.json -d restart
 wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh && ./tcp.sh
 wget -N --no-check-certificate "https://raw.githubusercontent.com/chiakge/Linux-NetSpeed/master/tcp.sh" && chmod +x tcp.sh && ./tcp.sh
